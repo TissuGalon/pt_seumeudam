e09d0b0be9516cc682bab2e286f2996d1337ebea
TissuGalon <parzivalxdd@gmail.com>
2026-04-06 00:41:32 +0700

    ganti db

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,17 @@
 import mysql from 'mysql2/promise';
 
 const dbConfig: mysql.PoolOptions = {
+  host: process.env.MYSQL_HOST || 'sql12.freesqldatabase.com',
+  user: process.env.MYSQL_USER || 'sql12822332',
+  password: process.env.MYSQL_PASSWORD || 'ybkZjbfgGr',
+  database: process.env.MYSQL_DATABASE || 'sql12822332',
+  port: parseInt(process.env.MYSQL_PORT || '3306'),
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+};
+
+/* const dbConfig: mysql.PoolOptions = {
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '',
@@ -9,7 +20,7 @@ const dbConfig: mysql.PoolOptions = {
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-};
+}; */
 
 // Create a pool instead of a single connection for better performance in Next.js Server environments
 export const pool = mysql.createPool(dbConfig);
